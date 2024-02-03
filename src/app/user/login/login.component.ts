897aa705fa6ea2ace51f318977d8e8010070ef6f
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-02-03 19:20:01 +0530

    login update

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ServiceService } from '../service.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule, ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  title = 'login';
+  isError: boolean = false;
+  errorMsg: string = '';
+  form!: FormGroup
+  constructor(private apiService: ServiceService, private router: Router) {
+    if(localStorage.getItem('loginInfo')) {
+      this.router.navigateByUrl('/home');
+    }
+  }
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required])
+    });
+  }
+  login() {
+    this.apiService.Login(this.form.value).subscribe((data) => {
+      localStorage.setItem('loginInfo', data.token);
+      this.router.navigateByUrl('/home');
+      this.isError = false;
+    }, (error) => {
+      this.isError = true;
+      this.errorMsg = error.error.msg;
+      setTimeout(() => {
+        this.isError = false;
+      }, 2000);
+    });
+  }
 }

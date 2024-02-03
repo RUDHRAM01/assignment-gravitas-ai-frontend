3ea6d9d379508191e51bcc7fc38bce262e8caf47
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-02-03 19:19:14 +0530

    service update

---
+++ src/app/user/service.service.ts
@@ -0,0 +1,92 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiceService {
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor(private httpClient: HttpClient) {}
+
+  Login(data: any): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/user/login`, data).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(error);
+      })
+    );
+  }
+
+  Register(data: any): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/user/register`, data).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(error);
+      })
+    );
+  }
+
+  createRecord(data: any): Observable<any> {
+    return this.httpClient
+      .post(`${this.apiUrl}/record/create`, data, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('loginInfo')}`,
+        }),
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  getRecords(): Observable<any> {
+    return this.httpClient
+      .get(`${this.apiUrl}/record/get`, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('loginInfo')}`,
+        }),
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  deleteRecord(id: number): Observable<any> {
+    return this.httpClient
+      .delete(`${this.apiUrl}/record/delete/${id}`, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('loginInfo')}`,
+        }),
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  updateRecord(id: number, data: any): Observable<any> {
+    return this.httpClient
+      .put(`${this.apiUrl}/record/update/${id}`, data, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('loginInfo')}`,
+        }),
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error);
+        })
+      );
+  }
+}

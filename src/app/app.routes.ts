84d664911c54f1135beacc42494f59de08dc6853
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-02-03 13:48:05 +0530

    update app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { IndexComponent } from './user/index/index.component';
+import { LandingComponent } from './user/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: LandingComponent
+  },
+  {
+    path: 'home', component: IndexComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'signup', component: SignupComponent
+  }
+];
